67c6eebc686a7d79ac7a9e184baeacc23b2cb5ac
Salawat9048 <salawat9048@outlook.com>
2024-05-19 22:35:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+print(type(123998))
+print(str("123998"))
+print(len("123998"))
+#print
+print(17 - 14)
+print(17 + 14)
+print(str("3"))
+print(str("31"))
+#print
+print(6 + 7 + 8)
+print(21 / 3)
+print(str("7"))
+#print
+a = "monday"
+b = "tuesday"
+print(a , b)
+#print
+a =2
+b =4
+c =7
+print(a * b + a * c)
+f =22
+print(f**3/2)
